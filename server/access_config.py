eb6552de3c9703699fcd21ee678ed847eb08af95
Asal Khaef <asal.khaef@gmail.com>
2024-01-03 14:05:49 +0330

    Add config files of users and dirs

---
+++ server/access_config.py
@@ -0,0 +1,19 @@
+USERS = [
+    {
+        "username": "Amirali",
+        "password": "1234",
+        "isPrivilaged": True
+    },
+    {
+        "username": "Asal",
+        "password": "5678",
+        "isPrivilaged": False
+    },
+    {
+        "username": "Arshia",
+        "password": "1010",
+        "isPrivilaged": True
+    }
+]
+
+PRIVATEPATH = ["./data/confidential", "./data/private.txt"]
